e93db105b2d36a4c9c1bc9abb44d4ca0b84bf1bc
Will Studabaker <will.studabaker@gmail.com>
2020-10-16 17:18:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,9 @@ function destructivelyRemoveFirstKitten(){
 function appendKitten(name){
   return [...kittens, name]
 }
+function prependKitten(name){
+  return [name, ...kittens]
+}
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1)
+}
